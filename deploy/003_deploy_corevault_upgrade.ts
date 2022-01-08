35fa6b2b946af7ebbed208b42c231a179f1a182b
notsegfault <notsegfault@gmail.com>
2022-01-08 13:48:16 -0500

    cleanup

--- deploy/003_deploy_corevault_upgrade.ts
+++ deploy/003_deploy_corevault_upgrade.ts
@@ -1,10 +1,9 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
 import { ethers, network } from "hardhat"
-import { CoreDAO, CoreDAOTreasury, CoreVaultV3, MockProxyAdmin } from "../types"
+import { CoreDAOTreasury, CoreVaultV3, MockProxyAdmin } from "../types"
 import { impersonate } from "../test/utilities"
 
-const startingCOREDAOAmount = 30000000
 const DEPLOYER = "0x5A16552f59ea34E44ec81E58b3817833E9fD5436"
 const ADMIN_PROXY = "0x9cb1eEcCd165090a4a091209E8c3a353954B1f0f"
 const VAULT = "0xC5cacb708425961594B63eC171f4df27a9c0d8c9"
